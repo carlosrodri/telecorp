f7652da9b8c16dadeaa0bfad98fb46b362e1a22d
Esteban Gutierrez <esteban.gutierrez@uptc.edu.co>
2019-11-28 10:22:02 -0500

    qasd

--- backend/logic/telecorpLogic.js
+++ backend/logic/telecorpLogic.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 let text = "";
 
 for (let index = 0; index < 30; index++) {
-    text += '1220' + index + '#' + '1010121735' + 'Cliente ' + index + '#' + 'boyacá' +
-        'Tunja' + '#' + 'cra 3 46-12' + '#' + index + '#' + '24/11/2019' + '#' + '24/11/2019' + '#'
-        + '20/10/2019' + '#' + '20/11/2019 ';
+    text += '1220' + index + '|' + '1010121735' + 'Cliente ' + index + '|' + 'boyacá' +
+        'Tunja' + '|' + 'cra 3 46-12' + '|' + index + '|' + '24/11/2019' + '|' + '24/11/2019' + '|'
+        + '20/10/2019' + '|' + '20/11/2019 ';
 
 }
 
@@ -14,6 +14,7 @@ fs.appendFile('nuevo_archivo.txt', text, (error) => {
     }
     console.log("un archivo ha sido creado");
 });
+
 
 function getJson() {
     fs.readFile('nuevo_archivo.txt', 'utf-8',(error, filecontent) => {
